81d03dbc89e891daf58c70eeda76c4b40dc36a87
ssmv713 <sshim9619@naver.com>
2023-03-16 11:23:11 +0900

    add: 배경이미지 추가

--- src/domain/Main/LandingView.tsx
+++ src/domain/Main/LandingView.tsx
@@ -6,17 +6,26 @@ import { Section3 } from "../section3/section3";
 import { Section3V2 } from "../section3/section3-v2";
 import { SlideSection } from "../SlideSection/SlideSecion";
 import { WorldFirst } from "../worldFirst/WorldFirst";
+import bg from "@/bg.png";
+import Image from "next/image";
+import { Stack } from "@mui/material";
 
 export const LandingView = () => {
   return (
     <div css={sx.root}>
-      <Section1 />
-      <Section2 />
-      <SlideSection />
-      <Section3 />
-      {/* <Section3V2 /> */}
-      <OneofaKind />
-      <WorldFirst />
+      <Stack css={sx.contentWrap}>
+        <Section1 />
+        <Section2 />
+        <SlideSection />
+        <Section3 />
+        {/* <Section3V2 /> */}
+        <OneofaKind />
+        <WorldFirst />
+      </Stack>
+
+      <div css={sx.bg}>
+        <Image src={bg} alt="bg" fill sizes="100" />
+      </div>
     </div>
   );
 };
@@ -24,5 +33,17 @@ export const LandingView = () => {
 const sx = {
   root: css`
     background-color: #000;
+    position: relative;
+  `,
+  contentWrap: css`
+    position: relative;
+    z-index: 5;
+  `,
+  bg: css`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
   `,
 };
