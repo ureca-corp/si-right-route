d778b14371644d536b08abe12d6e02581044632c
ssmv713 <sshim9619@naver.com>
2023-04-20 10:44:26 +0900

    fix :글자수정

--- src/domain/chapter1/chapter1.tsx
+++ src/domain/chapter1/chapter1.tsx
@@ -42,7 +42,7 @@ export const Chapter1 = () => {
             <Typography
               css={sx.title}
               variant="h5"
-            >{`전기차 배터리 핵심 부품 "이차전지 분리막"`}</Typography>
+            >{`전기차 배터리 핵심 부품 "배터리 분리막"`}</Typography>
             <Typography css={sx.text} variant="body2">
               {
                 "배터리 분리막은 전기차 시장의 활성화와 함께 그 생산량이 급격하게 증가하게 되었습니다.\n친환경 전기차라는 수식어와 다르게 그 뒤단에서는 심각한 환경오염을 유발할 수 있는 막대한 양의 분리막 폐기물이 소각되고 있습니다."
