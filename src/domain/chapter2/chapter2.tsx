ee00fe5628e9c414eb4bf5daf09e3b0d2a6ef33e
ssmv713 <sshim9619@naver.com>
2023-04-26 14:55:50 +0900

    fix: 용량축소

--- src/domain/chapter2/chapter2.tsx
+++ src/domain/chapter2/chapter2.tsx
@@ -39,8 +39,8 @@ export const Chapter2 = () => {
               loop
               playsInline
             />
-            <video autoPlay muted loop playsInline css={sx.video}>
-              <source src="/assets/ch2_video.mp4" />
+            <video autoPlay muted controls playsInline css={sx.video}>
+              <source src="/assets/ch2_video.mp4" type="video/mp4" />
             </video>
           </Stack>
           <Stack
